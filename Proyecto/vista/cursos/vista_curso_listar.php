da07b2ffc23ffb8a3b432b2d77a1e826a70bad76
Cristian E <66037173+Cristian1997@users.noreply.github.com>
2025-03-14 18:11:56 -0500

    Update vista_curso_listar.php

--- Proyecto/vista/cursos/vista_curso_listar.php
+++ Proyecto/vista/cursos/vista_curso_listar.php
@@ -1,8 +1,8 @@
-<script type="text/javascript" src="../js/cursos.js?rev=<?php echo time();?>"></script>
+<script type="text/javascript" src="../js/docente.js?rev=<?php echo time();?>"></script>
 <div class="col-md-12">
     <div class="box box-warning box-solid">
         <div class="box-header with-border">
-            <h3 class="box-title">CURSOS</h3>
+            <h3 class="box-title">DOCENTES</h3>
 
 
             <!-- /.box-tools -->
@@ -23,20 +23,28 @@
                             class="glyphicon glyphicon-plus"></i>Nuevo Registro</button>
                 </div>
             </div>
-            <table id="tabla_cursos" class="display responsive nowrap text-center" style="width:100%">
+            <table id="tabla_docente" class="display responsive nowrap text-center" style="width:100%">
                 <thead>
                     <tr>
                         <th>#</th>
-                        <th>Cursos</th>
-                        <th>Estado</th>
+                        <th>Documento</th>
+                        <th>Docente</th>
+                        <th>Fecha de nacimiento</th>
+                        <th>Telefono</th>
+                        <th>Email</th>
+                        <th>Sexo</th>
                         <th>Acci&oacute;n</th>
                     </tr>
                 </thead>
                 <tfoot>
                     <tr>
                         <th>#</th>
-                        <th>Cursos</th>
-                        <th>Estado</th>
+                        <th>Documento</th>
+                        <th>Docente</th>
+                        <th>Fecha de nacimiento</th>
+                        <th>Telefono</th>
+                        <th>Email</th>
+                        <th>Sexo</th>
                         <th>Acci&oacute;n</th>
                     </tr>
                 </tfoot>
@@ -49,26 +57,79 @@
 
 <form autocomplete="false" onsubmit="return false">
     <div class="modal fade" id="modal_registro" role="dialog">
-        <div class="modal-dialog modal-sm">
+        <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title"><b>Registrar Nueva asignatura</b></h4>
+                    <h4 class="modal-title"><b>Registro De Docentes</b></h4>
                 </div>
                 <div class="modal-body">
                     <div class="row">
-                        <div class="col-lg-12">
-                            <label for="">Nombre Asignatura</label>
-                            <input type="text" class="form-control" id="txt_nombres"
-                                placeholder="Ingrese nombres de Asignatura" maxlength="50"
-                                onkeypress="return soloLetras(event)"><br>
+                        <div class="col-lg-6">
+                            <label for="">Nombres</label>
+                            <input type="text" class="form-control" id="txt_nombres" placeholder="Ingrese nombres"
+                                maxlength="50" onkeypress="return soloLetras(event)"><br>
+                        </div>
+                        <div class="col-lg-6">
+                            <label for="">Apellidos</label>
+                            <input type="text" class="form-control" id="txt_apellidos" placeholder="Ingrese apellidos"
+                                maxlength="50" onkeypress="return soloLetras(event)"><br>
+                        </div>
+                        <div class="col-lg-6">
+                            <label for="">Documento</label>
+                            <input type="text" class="form-control" id="txt_documento" placeholder="Ingrese documento"
+                                onkeypress="return soloNumeros(event)"><br>
                         </div>
 
+                        <div class="col-lg-6">
+                            <label for="">Telefono</label>
+                            <input type="data" class="form-control" id="txt_telefono" placeholder="Ingrese telefono"
+                                onkeypress="return soloNumeros(event)"><br>
+
+                        </div>
+
+                        <div class="col-lg-6">
+                            <label for="">Fecha de Nacimiento</label>
+                            <input type="date" class="form-control" id="txt_fecha"><br>
+                        </div>
+                        <div class="col-lg-6">
+                            <label for="">Sexo</label>
+                            <select class="js-example-basic-single" name="state" id="cbm_sexo" style="width:100%;">
+                                <option value="M">MASCULINO</option>
+                                <option value="F">FEMENINO</option>
+                            </select><br><br>
+                        </div>
+                        <div class="col-lg-12" style="text-align:center">
+                            <b>DATOS DEL USUARIO</b><br><br>
+                        </div>
+
+                        <div class="col-lg-4">
+                            <label for="">Usuario</label>
+                            <input type="text" class="form-control" id="txt_usu" placeholder="Ingrese usuario"><br>
+                        </div>
+
+                        <div class="col-lg-4">
+                            <label for="">Contrase&ntilde;a</label>
+                            <input type="password" class="form-control" id="txt_contra"
+                                placeholder="Ingrese contraseña"><br>
+                        </div>
+                        <div class="col-lg-4">
+                            <label for="">Confirmar Contrase&ntilde;a</label>
+                            <input type="password" class="form-control" id="txt_contra1"
+                                placeholder="Ingrese contraseña"><br>
+                        </div>
+
+                        <div class="col-lg-12">
+                            <label for="">Email</label>
+                            <input type="text" class="form-control" id="txt_email" placeholder="Ingrese email"><br>
+                            <label for="" id="emailOK" style="color:red;"></label>
+                            <input type="text" id="validar_email" hidden>
+                        </div>
 
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button class="btn btn-primary" onclick="Registrar_asignatura()"><i
+                    <button class="btn btn-primary" onclick="Registrar_Docente()"><i
                             class="fa fa-check"><b>&nbsp;Registrar</b></i></button>
                     <button type="button" class="btn btn-danger" data-dismiss="modal"><i
                             class="fa fa-close"><b>&nbsp;Cerrar</b></i></button>
@@ -80,26 +141,86 @@
 
 <form autocomplete="false" onsubmit="return false">
     <div class="modal fade" id="modal_editar" role="dialog">
-        <div class="modal-dialog modal-sm">
+        <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title"><b>Editar Asignatura</b></h4>
+                    <h4 class="modal-title"><b>Editar Datos Del Docentes</b></h4>
                 </div>
                 <div class="modal-body">
                     <div class="row">
-                        <div class="col-lg-12">
-                            <input type="text" id="id_asignatura" hidden>
-                            <label for="">Nombre de Asignatura</label>
+                        <div class="col-lg-6">
+                            <input type="text" id="id_docente" hidden>
+                            <label for="">Nombres</label>
                             <input type="text" class="form-control" id="txt_nombres_editar"
                                 placeholder="Ingrese nombres" maxlength="50" onkeypress="return soloLetras(event)"><br>
                         </div>
+                        <div class="col-lg-6">
+                            <label for="">Apellidos</label>
+                            <input type="text" class="form-control" id="txt_apellidos_editar"
+                                placeholder="Ingrese apellidos" maxlength="50"
+                                onkeypress="return soloLetras(event)"><br>
+                        </div>
+                        <div class="col-lg-6">
+                            <label for="">Documento</label>
+                            <input type="text" id="txt_documento_editar_actual" hidden><input type="text"
+                                class="form-control" id="txt_documento_editar_nuevo" placeholder="Ingrese documento"
+                                onkeypress="return soloNumeros(event)"><br>
+                        </div>
+                        <div class="col-lg-6">
+                            <label for="">Telefono</label>
+                            <input type="data" class="form-control" id="txt_telefono_editar"
+                                placeholder="Ingrese telefono" onkeypress="return soloNumeros(event)"><br>
 
+                        </div>
+
+
+                        <div class="col-lg-6">
+                            <label for="">Sexo</label>
+                            <select class="js-example-basic-single" name="state" id="cbm_sexo_editar"
+                                style="width:100%;">
+                                <option value="M">MASCULINO</option>
+                                <option value="F">FEMENINO</option>
+                            </select><br><br>
+                        </div>
+                        <div class="col-lg-6">
+                            <label for="">Fecha de Nacimiento</label>
+                            <input type="date" class="form-control" id="txt_fecha_editar"><br>
+                        </div>
+
+
+                        <div class="col-lg-12">
+                            <label for="">Email</label>
+                            <input type="text" class="form-control" id="txt_email_editar"
+                                placeholder="Ingrese email"><br>
+                            <label for="" id="emailOK_editar" style="color:red;"></label>
+                            <input type="text" id="validar_email_editar" hidden>
+                        </div>
+
+
+                        <div class="col-lg-12" style="text-align:center">
+                            <b>DATOS DEL USUARIO</b><br><br>
+                        </div>
+
+                        <div class="col-lg-6">
+                            <input type="text" id="id_usuario" hidden>
+                            <label for="">Usuario</label>
+                            <input type="text" class="form-control" id="txt_usu_editar" placeholder="Ingrese usuario"
+                                disabled><br>
+                        </div>
+
+
+
+
+                        <div class="col-lg-6 text-center">
+                            <label for="" class="text-center">Estado de Cuenta</label><br>
+                            <span class="bt btn btn-success" id="status" style="width: 120px; height: 35px;"></span>
+                        </div>
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button class="btn btn-primary" onclick="Editar_Asignaturas()"><i
-                            class="fa fa-check"><b>&nbsp;Editar Registro</b></i></button>
+                    <button class="btn btn-primary" onclick="Editar_Docente()"><i class="fa fa-check"><b>&nbsp;Editar
+                                Registro</b></i></button>
                     <button type="button" class="btn btn-danger" data-dismiss="modal"><i
                             class="fa fa-close"><b>&nbsp;Cerrar</b></i></button>
                 </div>
@@ -110,7 +231,7 @@
 
 <script>
 $(document).ready(function() {
-    listar_cursos();
+    listar_docente();
 
     $('.js-example-basic-single').select2();
     $("#modal_registro").on('shown.bs.modal', function() {

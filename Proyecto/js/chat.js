81ec708a89edd646e39afdbdea8ea146273b1c36
Cristian E <66037173+Cristian1997@users.noreply.github.com>
2025-03-14 14:36:50 -0500

    Update chat.js

--- Proyecto/js/chat.js
+++ Proyecto/js/chat.js
@@ -51,17 +51,15 @@ tabla_chat_estudiantes = $("#tabla_chat_estudiantes").DataTable({
 "language": idioma_espanol,
 select: true
 });
-// Ocultar el filtro de búsqueda de DataTables
 document.getElementById("tabla_chat_estudiantes_filter").style.display = "none";
 
-// Asociar evento al nuevo input para la búsqueda global
 $('input#global_filter').on('keyup', function () {
-    filterGlobal($(this).val()); // Llama a la función filterGlobal con el valor del input
+    filterGlobal($(this).val()); 
 });
 
-// Función para filtrar globalmente
+// Función para filtrar busqueda
 function filterGlobal(value) {
-    tabla_chat_estudiantes.search(value).draw(); // Realiza la búsqueda global y redibuja la tabla
+    tabla_chat_estudiantes.search(value).draw(); 
 }
 
 tabla_chat_estudiantes.on('draw.dt', function() {
@@ -83,13 +81,11 @@ function nuevo_chat_estudiantes() {
 }
 
 $(document).ready(function() {
-    // Función para abrir el modal
     function archivo_documento() {
         $("#modal_archivo_documento").modal({ backdrop: 'static', keyboard: false });
         $("#modal_archivo_documento").modal('show');
     }
 
-    // Agregar evento click al enlace "Elegir documento"
     $("#elegir_documento").click(function() {
         archivo_documento();
     });
@@ -99,12 +95,10 @@ $(document).ready(function() {
 
 
 $(document).ready(function() {
-    // Función para abrir el modal
     function archivo_imagen() {
         $("#modal_archivo_imagen").modal({ backdrop: 'static', keyboard: false });
         $("#modal_archivo_imagen").modal('show');
     }
-     // Agregar evento click al enlace "Elegir documento"
      $("#elegir_imagen").click(function() {
         archivo_imagen();
     });
@@ -117,14 +111,14 @@ function previewImage() {
 
     reader.onloadend = function() {
         preview.src = reader.result;
-        preview.style.display = 'block'; // Mostrar la imagen previa
+        preview.style.display = 'block'; 
     }
 
     if (file) {
-        reader.readAsDataURL(file); // Convertir la imagen a un URL base64
+        reader.readAsDataURL(file); 
     } else {
-        preview.src = ''; // Si no se selecciona ninguna imagen, limpiar la vista previa
-        preview.style.display = 'none'; // Ocultar la imagen previa
+        preview.src = ''; 
+        preview.style.display = 'none'; 
     }
 }
 
@@ -137,13 +131,13 @@ function nuevo_chat_docentes() {
 
 $(document).ready(function() {
     $(document).on('click', '.abrir_imagen', function() {
-        // Extraer la URL de la imagen adjunta
+       
         var imageUrl = $(this).closest('.attachment').find('img').attr('src');
         
-        // Insertar la imagen en el modal
+      
         $('#modal_archivo_imagen_ver .modal-body').html('<img src="' + imageUrl + '" style="max-width: 100%; height: auto;">');
         
-        // Mostrar el modal
+  
         $("#modal_archivo_imagen_ver").modal({ backdrop: 'static', keyboard: false });
         $("#modal_archivo_imagen_ver").modal('show');
     });
@@ -153,12 +147,10 @@ $(document).ready(function() {
 //------------registros-----------------------------
 
 
-// Define la función que se ejecutará automáticamente cada cierto intervalo de tiempo para actualizar la vista del chat
 function actualizarVistaChat() {
-    listar_chat_directo(); // Llamar a la función para listar los comentarios directos
+    listar_chat_directo(); 
 }
 
-// Configura el intervalo para llamar a actualizarVistaChat() cada 10 segundos (10000 milisegundos)
 setInterval(actualizarVistaChat, 3000 );
 
 function responder_chat(){
@@ -167,10 +159,9 @@ function responder_chat(){
     var id_usuario = $("#txtidusuario").val();
 
 
-  // Verificar si id_chat y mensaje están vacíos
   if (!id_chat || !comentario || !id_usuario) {
     Swal.fire("Mensaje De Error", "Seleccione un chat y escriba un mensaje", "warning");
-    return; // Salir de la función si los datos están vacíos
+    return; 
 }
  
 
@@ -184,7 +175,6 @@ function responder_chat(){
         }
     }).done(function(resp){
         if (resp > 0) {
-            // Aquí estaba el mensaje de confirmación que hemos eliminado
             listar_chat_directo();
             $("#txt_comentario").val("");
         }
@@ -205,7 +195,7 @@ function nuevo_chat(){
 
     if ( id_chat_nuevo == null || id_usuario == null ) {
         Swal.fire("Mensaje De Advertencia", "Datos Vacios","warning");
-        return; // Añadimos este return para salir de la función si los datos están vacíos
+        return; 
     }
 
     $.ajax({
@@ -217,13 +207,11 @@ function nuevo_chat(){
         }
     }).done(function (resp) {
         if (resp != "") {
-            // Comprobamos si la respuesta es -1, que indica que el chat ya existe
             if (resp == -1) {
                 Swal.fire("Mensaje De Advertencia", "Ya existe un chat iniciado con este usuario. Por favor, seleccione otro usuario.", "warning");
                 return;
             }
     
-            // Comprobamos si la respuesta es 1, que indica que se registró correctamente
             if (resp == 1) {
                 Swal.fire("Mensaje De Confirmacion", "Datos correctamente, Nuevo Chat Registrado", "success")
                     .then((value) => {
@@ -233,7 +221,6 @@ function nuevo_chat(){
             }
         }
         
-        // Si la respuesta no es -1 ni 1, mostramos mensaje de error
         Swal.fire("Mensaje De Error", "Lo sentimos, no se pudo completar el registro", "error");
     });
 }
@@ -242,12 +229,11 @@ function nuevo_chat(){
 
 function mensaje_visto(){
     var id_chat_visto = $("#id_chat_visto").val();
-    // Verifica si el campo está vacío o solo contiene espacios en blanco
     if (id_chat_visto.trim().length === 0) { 
-        return; // Salir de la función si los datos están vacíos
+        return; 
     }
 
-    // Realiza una solicitud AJAX para marcar un chat como visto
+   
     $.ajax({
         url:"../controlador/chat/controlador_chat_visto.php",
         type:"POST",
@@ -255,15 +241,13 @@ function mensaje_visto(){
             id_chat_visto:id_chat_visto
         }
     }).done(function (resp) {
-        // Maneja la respuesta de la solicitud AJAX
+      
         if (resp != "") {
-            // Comprobamos si la respuesta es -1, que indica que el chat ya existe
             if (resp == -1) {
-                return; // Salir de la función si el chat ya está abierto
+                return; 
             }
-            // Comprobamos si la respuesta es 1, que indica que se registró correctamente
             if (resp == 1) {
-                return; // Salir de la función si el chat se registró correctamente
+                return; 
             }
         }
     });
@@ -271,12 +255,10 @@ function mensaje_visto(){
 
 function mensaje_abierto(){
     var id_chat_abierto = $("#id_chat_no_abierto").val();
-    // Verifica si el campo está vacío o solo contiene espacios en blanco
     if (id_chat_abierto.trim().length === 0) { 
-        return; // Salir de la función si los datos están vacíos
+        return; 
     }
 
-    // Realiza una solicitud AJAX para marcar un chat como abierto
     $.ajax({
         url:"../controlador/chat/controlador_chat_abierto.php",
         type:"POST",
@@ -284,15 +266,12 @@ function mensaje_abierto(){
             id_chat_abierto:id_chat_abierto
         }
     }).done(function (resp) {
-        // Maneja la respuesta de la solicitud AJAX
         if (resp != "") {
-            // Comprobamos si la respuesta es -1, que indica que el chat ya está visto
             if (resp == -1) {
-                return; // Salir de la función si el chat ya está visto
+                return;
             }
-            // Comprobamos si la respuesta es 1, que indica que se registró correctamente
             if (resp == 1) {
-                return; // Salir de la función si el chat se registró correctamente
+                return; 
             }
         }
     });
@@ -305,10 +284,9 @@ function enviar_archivo() {
     var archivoInput = $("#txt_archivo_subir")[0];
     var archivo = archivoInput.files[0];
 
-  // Verificar si id_chat y mensaje están vacíos
   if (!id_chat) {
     Swal.fire("Mensaje De Error", "Seleccione un chat y escriba un mensaje", "error");
-    return; // Salir de la función si los datos están vacíos
+    return; 
 }
 
     if (!archivo) {
@@ -371,10 +349,9 @@ function enviar_imagen() {
     var archivoInput = $("#txt_imagen_subir")[0];
     var archivo = archivoInput.files[0];
 
-  // Verificar si id_chat y mensaje están vacíos
   if (!id_chat) {
     Swal.fire("Mensaje De Error", "Seleccione un chat y escriba un mensaje", "error");
-    return; // Salir de la función si los datos están vacíos
+    return; 
 }
 
     if (!archivo) {
@@ -405,7 +382,7 @@ if (fileSize > maxSize) {
     var nombreAleatorio = Math.random().toString(36).substring(7);
     var nombrearchivo = nombreAleatorio + "_" + fecha.getDate() + "" + (fecha.getMonth() + 1) + "" + fecha.getFullYear() + "" + fecha.getMinutes() + "" + fecha.getMilliseconds() + "." + extension;
 
-    // Reemplazar los caracteres no deseados con "_"
+    // Reemplazar los caracteres no deseados"
     caracteresNoDeseados.forEach(function(caracter) {
         nombrearchivo = nombrearchivo.replace(new RegExp("\\" + caracter, "g"), "_");
     });
@@ -439,23 +416,18 @@ if (fileSize > maxSize) {
 
 //----------------chat---------------
 
-// Manejador de eventos clic para los botones de abrir conversación
 $(document).on('click', '.abrir_chat', function() {
     var id_conversacion = $(this).data('id');
     var imagenUsuario = $(this).closest('li').find('.contacts-list-img').attr('src');
     var nombreUsuario = $(this).closest('li').find('.contacts-list-name').text();
 
-    // Establecer el valor del campo #id_chat
     $('#id_chat').val(id_conversacion);
 
-    // Limpiar el valor del campo #id_chat_no_abierto
     $('#id_chat_no_abierto').val('');
 
-    // Reflejar la imagen y el nombre del usuario
     $('#message_user_image').attr('src', imagenUsuario);
     $('#message_user_name').text(nombreUsuario);
 
-  // Mostrar el div con la clase "box-tools pull-right"
   $('.box-tools.pull-right').show();
 
     listar_chat_directo();
@@ -477,7 +449,6 @@ function listar_chat_estudiantes_es() {
         let data = JSON.parse(resp);
         var totalConversaciones = 0;
 
-        // Limpia el contenido actual antes de mostrar los nuevos datos
         $("#lista_conversaciones").empty();
 
         if (data.data.length > 0) {
@@ -492,7 +463,6 @@ function listar_chat_estudiantes_es() {
                 console.log("id_segundario:", conversacion.id_segundario);
                 console.log("id_usuario_es:", id_usuario_es);
 
-                // Verifica si el usuario actual coincide con id_principal o id_segundario
                 if (conversacion.id_principal == id_usuario_es) {
                     fotoUsuario = conversacion.foto_usuario_secundario;
                     nombreUsuario = conversacion.nombre_usuario_secundario;
@@ -520,24 +490,20 @@ function listar_chat_estudiantes_es() {
 
             
                 
-                // Incrementa el contador de conversaciones
                 totalConversaciones++;
             });
 
-            // Agrega todas las conversaciones al contenedor con el id "lista_conversaciones"
             $("#lista_conversaciones").html(listaConversacionesHTML);
         }
 
-        // Muestra el total de conversaciones en el span con el id "txt_contador_conversaciones"
         $("#txt_contador_conversaciones").text(totalConversaciones);
     });
 }
 
 
 
-// Asociar evento al nuevo input para la búsqueda global por nombre en las conversaciones
 $('input#global_filter_nombre_conversaciones').on('keyup', function () {
-    filterGlobalNombreConversaciones($(this).val()); // Llama a la función filterGlobalNombreConversaciones con el valor del input
+    filterGlobalNombreConversaciones($(this).val()); 
 });
 
 // Función para filtrar globalmente por nombre en las conversaciones
@@ -568,22 +534,18 @@ function listar_chat_directo() {
     }).done(function(resp) {
         let data = JSON.parse(resp);
 
-        // Limpiar el contenido actual antes de mostrar los nuevos datos
         $(".direct-chat-messages").empty();
 
         if (data.data && data.data.length > 0) {
             data.data.forEach(function(comentario) {
                 var esUsuarioActual = comentario.id_comentario == id_usuario_es;
             
-                // Solo agregar el comentario a la lista de mensajes no vistos si no fue enviado por el usuario actual
                 if (!esUsuarioActual && comentario.Estado === 'NO VISTO') {
                     ultimoMensajeNoVisto.push(comentario.ID);
                 }
             
-                // Formatear la fecha
                 var fechaFormateada = formatearFecha(comentario.Fecha);
             
-                // Construir el HTML para el comentario
                 var comentarioHTML = '<div class="direct-chat-msg';
                 comentarioHTML += esUsuarioActual ? ' right">' : '">'; // Agregar la clase 'right' si es el usuario actual
                 comentarioHTML += '<div class="direct-chat-info clearfix">';
@@ -593,21 +555,16 @@ function listar_chat_directo() {
                 comentarioHTML += '<img class="direct-chat-img" src="../' + comentario.foto + '" alt="Message User Image">';
                 comentarioHTML += '<div class="direct-chat-text">';
             
-                // Agregar el mensaje de texto
                 comentarioHTML += comentario.Comentario;
             
-                // Verificar si hay archivo adjunto
                 if (comentario.archivo) {
                     comentarioHTML += '<div class="attachment">';
                     comentarioHTML += '<h4>Archivo:</h4>';
                     
-                    // Extraer solo el nombre del archivo
                     var nombreArchivo = comentario.archivo.split('/').pop();
                     
-                    // Mostrar el nombre del archivo
                     comentarioHTML += '<p>' + nombreArchivo + '</p>';
                     
-                    // Verificar si el archivo es una imagen
                     if (/\.(jpeg|jpg|png|gif)$/i.test(nombreArchivo)) {
                         comentarioHTML += '<img src="../' + comentario.archivo + '" alt="Archivo adjunto" style="max-width: 100px;">'; // Ajusta el valor de max-width
                         comentarioHTML += '<div class="clearfix">';
@@ -616,64 +573,53 @@ function listar_chat_directo() {
                         comentarioHTML += '</div>';
                         comentarioHTML += '</div>';
                     } else {
-                        // Si no es una imagen, mostrar un enlace de descarga con estilo
                         comentarioHTML += '<a href="../' + comentario.archivo + '" download="' + nombreArchivo + '" style="color: black; font-weight: bold; text-decoration: none;">Descargar archivo</a>';
                     }
                     
                     comentarioHTML += '</div>';
                 }
             
-                comentarioHTML += '</div>'; // Cerrar direct-chat-msg
+                comentarioHTML += '</div>'; 
             
-                // Agregar el comentario al contenedor de mensajes directos
                 $(".direct-chat-messages").append(comentarioHTML);
             });
         } else {
-            // Mostrar un mensaje indicando que aún no se ha iniciado una conversación
             $(".direct-chat-messages").html('<div class="alert alert-warning">Aún no se ha iniciado una conversación.</div>');
         }
 
-        // Asignar los IDs de los últimos mensajes no vistos al campo id_chat_visto
         if (ultimoMensajeNoVisto.length > 0) {
             $('#id_chat_visto').val(ultimoMensajeNoVisto.join(',')); // Convertir el array a una cadena separada por comas
         }
 
         setTimeout(function() {
             mensaje_visto();
-        }, 1000); // Aquí puedes especificar el tiempo de espera en milisegundos (en este ejemplo, se espera 1000 milisegundos o 1 segundos)
+        }, 1000); 
     });
 }
 
 
 function formatearFecha(fecha) {
-    // Convertir la cadena de fecha a objeto Date
     var fechaObjeto = new Date(fecha);
 
-    // Array de nombres de los meses completos
     var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
 
-    // Obtiene el día del mes, mes, año y hora
     var dia = fechaObjeto.getDate();
     var mes = meses[fechaObjeto.getMonth()];
-    var año = fechaObjeto.getFullYear(); // Obtener el año
+    var año = fechaObjeto.getFullYear(); 
     var hora = fechaObjeto.getHours();
     var minutos = fechaObjeto.getMinutes();
     var ampm = hora >= 12 ? 'pm' : 'am';
 
-    // Convertir hora a formato de 12 horas
     hora = hora % 12;
-    hora = hora ? hora : 12; // Si es 0, lo convierte a 12
+    hora = hora ? hora : 12; 
 
-    // Agregar un cero inicial si los minutos son menores a 10
     minutos = minutos < 10 ? '0' + minutos : minutos;
 
-    // Construir la cadena de fecha formateada
     var fechaFormateada = dia + ' ' + mes + ' ' + año + ' ' + hora + ':' + minutos + ' ' + ampm;
 
     return fechaFormateada;
 }
 
-// Evento clic para el botón de desplazamiento hacia abajo
 $('#scrollDownButton').on('click', function() {
     $(".direct-chat-messages").scrollTop($(".direct-chat-messages")[0].scrollHeight);
  
@@ -693,7 +639,6 @@ function listar_integrantes_salon_estudiantes() {
         let data = JSON.parse(resp);
         var totalIntegrantes = 0;
 
-        // Limpia el contenido actual antes de mostrar los nuevos datos
         $("#lista_estudiantes").empty();
 
         if (data.data.length > 0) {
@@ -709,7 +654,6 @@ function listar_integrantes_salon_estudiantes() {
             });
         }
 
-        // Muestra el total de integrantes en el span con el id "txt_contador_integrantes"
         $("#txt_contador_integrantes").text(totalIntegrantes);
     });
 }
@@ -727,7 +671,6 @@ function listar_integrantes_salon_docentes() {
         let data = JSON.parse(resp);
         var totalIntegrantes = 0;
 
-        // Limpia el contenido actual antes de mostrar los nuevos datos
         $("#lista_docentes").empty();
 
         if (data.data.length > 0) {
@@ -743,7 +686,6 @@ function listar_integrantes_salon_docentes() {
             });
         }
 
-        // Muestra el total de integrantes en el span con el id "txt_contador_integrantes"
         $("#txt_contador_integrantes").text(totalIntegrantes);
     });
 }
@@ -753,7 +695,6 @@ $(document).on("click", ".iniciar_chat", function() {
     var idUsuario = $(this).data("id");
     $("#id_nuevo_chat").val(idUsuario);
 
-      // Llamar a la función nuevo_chat()
       nuevo_chat();
 
 });

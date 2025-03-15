3c30c7f50089a3a3897b17d0f60a533667926d13
Cristian E <66037173+Cristian1997@users.noreply.github.com>
2025-03-15 00:08:25 -0500

    Update controlador_registrar_estudiantes.php

--- Proyecto/controlador/calificaciones/controlador_registrar_estudiantes.php
+++ Proyecto/controlador/calificaciones/controlador_registrar_estudiantes.php
@@ -1,11 +1,12 @@
 <?php
 require '../../modelo/modelo_calificaciones.php';
-$MC             = new Modelo_Calificaciones();
+$MC = new Modelo_Calificaciones();
+
 $id_estudiante = htmlspecialchars($_POST['id_estudiante'], ENT_QUOTES, 'UTF-8');
 $id_docente = htmlspecialchars($_POST['id_docente'], ENT_QUOTES, 'UTF-8');
 $arreglo        = explode(",", $id_estudiante); //se paro los datos
 for ($i = 0; $i < count($arreglo); $i++) {
     $consulta = $MC->Registrar_Estudiantes($arreglo[$i],$id_docente);
 }
-
 echo $consulta;
+?>

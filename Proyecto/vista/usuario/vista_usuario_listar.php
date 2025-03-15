547bf8e5da063efbb8e5c890a7ddbbb82c156103
Cristian E <66037173+Cristian1997@users.noreply.github.com>
2025-03-15 11:59:16 -0500

    Update vista_usuario_listar.php

--- Proyecto/vista/usuario/vista_usuario_listar.php
+++ Proyecto/vista/usuario/vista_usuario_listar.php
@@ -2,55 +2,53 @@
 <div class="col-md-12">
     <div class="box box-warning box-solid">
         <div class="box-header with-border">
-          <h3 class="box-title">USUARIOS</h3>
-
-
-          <!-- /.box-tools -->
-      </div>
-      <!-- /.box-header -->
-      <div class="box-body">
-        <div class="form-group">
-            <div class="col-lg-10">
-                <div class="input-group">
-                    <input type="text" class="global_filter form-control" id="global_filter" placeholder="Ingresar dato a buscar">
-                    <span class="input-group-addon"><i class="fa fa-search"></i></span>
-                </div>
-                <br>
-            </div>
-            <div class="col-lg-2">
-                <button class="btn btn-danger" style="width:100%" onclick="AbrirModalRegistro()"><i class="glyphicon glyphicon-plus"></i>Nuevo Registro</button>
-            </div>
+            <h3 class="box-title">USUARIOS</h3>
+            <!-- /.box-tools -->
         </div>
-        <table id="tabla_usuario" class="display responsive nowrap" style="width:100%">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Usuario</th>
-                    <th>Email</th>
-                    <th>Rol</th>
-                    <th>Sexo</th>
-                    <th>Estatus</th> 
-                    <th>Foto</th>
-                    <th>Acci&oacute;n</th>
-                </tr>
-            </thead>
-            <tfoot>
-                <tr>
-                    <th>#</th>
-                    <th>Usuario</th>
-                    <th>Email</th>
-                    <th>Rol</th>
-                    <th>Sexo</th>
-                    <th>Estatus</th> 
-                    <th>Foto</th>
-                    <th>Acci&oacute;n</th>
-                </tr>
-            </tfoot>
-        </table>
+        <!-- /.box-header -->
+        <div class="box-body">
+            <div class="form-group">
+                <div class="col-lg-10">
+                    <div class="input-group">
+                        <input type="text" class="global_filter form-control" id="global_filter" placeholder="Ingresar dato a buscar">
+                        <span class="input-group-addon"><i class="fa fa-search"></i></span>
+                    </div>
+                    <br>
+                </div>
+                <div class="col-lg-2">
+                    <button class="btn btn-danger" style="width:100%" onclick="AbrirModalRegistro()"><i class="glyphicon glyphicon-plus"></i>Nuevo Registro</button>
+                </div>
+            </div>
+            <table id="tabla_usuario" class="display responsive nowrap" style="width:100%">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Usuario</th>
+                        <th>Email</th>
+                        <th>Rol</th>
+                        <th>Sexo</th>
+                        <th>Estatus</th>
+                        <th>Foto</th>
+                        <th>Acci&oacute;n</th>
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                        <th>#</th>
+                        <th>Usuario</th>
+                        <th>Email</th>
+                        <th>Rol</th>
+                        <th>Sexo</th>
+                        <th>Estatus</th>
+                        <th>Foto</th>
+                        <th>Acci&oacute;n</th>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
+        <!-- /.box-body -->
     </div>
-    <!-- /.box-body -->
-</div>
-<!-- /.box -->
+    <!-- /.box -->
 </div>
 <form autocomplete="false" onsubmit="return false">
     <div class="modal fade" id="modal_registro" role="dialog">
@@ -62,51 +60,66 @@
                 </div>
                 <div class="modal-body">
                     <div class="row ">
+                        
                         <div class="col-lg-12">
                             <label for="">Usuario</label>
-                            <input type="text" class="form-control" id="txt_usu" placeholder="Ingrese usuario"><br>
+                            <input type="text" class="form-control" id="txt_usu" placeholder="Ingrese usuario">
+                            <br>
                         </div>
+                        
                         <div class="col-lg-12">
                             <label for="">Email</label>
                             <input type="text" class="form-control" id="txt_email" placeholder="Ingrese email">
                             <label for="" id="emailOK" style="color:red;"></label>
                             <input type="text" id="validar_email" hidden>
                         </div>
+                        
                         <div class="col-lg-12">
                             <label for="">Contrase&ntilde;a</label>
-                            <input type="password" class="form-control" id="txt_con1" placeholder="Ingrese contrase&ntilde;a"><br>
+                            <input type="password" class="form-control" id="txt_con1" placeholder="Ingrese contrase&ntilde;a">
+                            <br>
                         </div>
+                        
                         <div class="col-lg-12">
                             <label for="">Repita la Contrase&ntilde;a</label>
-                            <input type="password" class="form-control" id="txt_con2" placeholder="Repita contrase&ntilde;a"><br>
+                            <input type="password" class="form-control" id="txt_con2" placeholder="Repita contrase&ntilde;a">
+                            <br>
                         </div>
+                        
                         <div class="col-lg-12">
                             <label for="">Sexo</label>
                             <select class="js-example-basic-single" name="state" id="cbm_sexo" style="width:100%;">
                                 <option value="M">MASCULINO</option>
                                 <option value="F">FEMENINO</option>
-                            </select><br><br>
+                            </select>
+                            <br>
+                            <br>
                         </div>
+                        
                         <div class="col-lg-12">
                             <label for="">Rol</label>
                             <select class="js-example-basic-single" name="state" id="cbm_rol" style="width:100%;">
-                            </select><br><br>
+                            </select>
+                            <br>
+                            <br>
                         </div>
-                        <div class="col-lg-6"><br>
-                           
-                           <label for="">Foto</label><br>
-                           <input type="file" id="img_foto">
-                       </div>
-
-                   </div>
-               </div>
-               <div class="modal-footer">
-                <button class="btn btn-primary" onclick="Registrar_Usuario()"><i class="fa fa-check"><b>&nbsp;Registrar</b></i></button>
-                <button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fa fa-close"><b>&nbsp;Cerrar</b></i></button>
+                        
+                        <div class="col-lg-6">
+                            <br>
+                            <label for="">Foto</label>
+                            <br>
+                            <input type="file" id="img_foto">
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-primary" onclick="Registrar_Usuario()"><i class="fa fa-check"><b>&nbsp;Registrar</b></i></button>
+                    <button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fa fa-close"><b>&nbsp;Cerrar</b></i></button>
+                </div>
             </div>
         </div>
     </div>
-</div>
 </form>
 
 <form autocomplete="false" onsubmit="return false">
@@ -118,46 +131,52 @@
                     <h4 class="modal-title"><b>Editar Datos Del Usuario</b></h4>
                 </div>
                 <div class="modal-body">
-                    
-<div class="row"> 
-                    <div class="col-lg-12">
-                        <input type="text" id=txtidusuario hidden>
-                        <label for="">Usuario</label>
-                        <input type="text" class="form-control" id="txtusu_editar" placeholder="Ingrese usuario"disabled><br>
-                    </div>
 
-                    <div class="col-lg-12">
-                        <label for="">Email</label>
-                        <input type="text" class="form-control" id="txt_email_editar" placeholder="Ingrese email">
-                        <label for="" id="emailOK_editar" style="color:red;"></label>
-                        <input type="text" id="validar_email_editar" hidden>
-                    </div>
+                    <div class="row">
+                        
+                        <div class="col-lg-12">
+                            <input type="text" id=txtidusuario hidden>
+                            <label for="">Usuario</label>
+                            <input type="text" class="form-control" id="txtusu_editar" placeholder="Ingrese usuario" disabled>
+                            <br>
+                        </div>
 
-                    <div class="col-lg-12">
-                        <label for="">Sexo</label>
-                        <select class="js-example-basic-single" name="state" id="cbm_sexo_editar" style="width:100%;">
-                            <option value="M">MASCULINO</option>
-                            <option value="F">FEMENINO</option>
-                        </select><br><br>
+                        <div class="col-lg-12">
+                            <label for="">Email</label>
+                            <input type="text" class="form-control" id="txt_email_editar" placeholder="Ingrese email">
+                            <label for="" id="emailOK_editar" style="color:red;"></label>
+                            <input type="text" id="validar_email_editar" hidden>
+                        </div>
+
+                        <div class="col-lg-12">
+                            <label for="">Sexo</label>
+                            <select class="js-example-basic-single" name="state" id="cbm_sexo_editar" style="width:100%;">
+                                <option value="M">MASCULINO</option>
+                                <option value="F">FEMENINO</option>
+                            </select>
+                            <br>
+                            <br>
+                        </div>
+                        
+                        <div class="col-lg-12">
+                            <label for="">Rol</label>
+                            <select class="js-example-basic-single" name="state" id="cbm_rol_editar" style="width:100%;">
+                            </select>
+                            <br>
+                            <br>
+                        </div>
+                        
                     </div>
-                    <div class="col-lg-12">
-                        <label for="">Rol</label>
-                        <select class="js-example-basic-single" name="state" id="cbm_rol_editar" style="width:100%;">
-                        </select><br><br>
-                    </div>
- 
-                    </div>
-                    </div>
+                </div>
                 <div class="modal-footer">
                     <button class="btn btn-primary" onclick="Modificar_Usuario()"><i class="fa fa-check"><b>&nbsp;Modificar</b></i></button>
                     <button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fa fa-close"><b>&nbsp;Cerrar</b></i></button>
                 </div>
-              
             </div>
-       
         </div>
     </div>
 </form>
+
 <script>
     $(document).ready(function() {
         listar_usuario();

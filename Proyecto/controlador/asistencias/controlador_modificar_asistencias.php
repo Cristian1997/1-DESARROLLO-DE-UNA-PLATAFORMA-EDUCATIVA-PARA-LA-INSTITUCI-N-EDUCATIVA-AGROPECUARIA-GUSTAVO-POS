9f5cf715bcca86608d978b0c4f55e600bda198ed
Cristian E <66037173+Cristian1997@users.noreply.github.com>
2025-03-15 00:03:22 -0500

    Update controlador_modificar_asistencias.php

--- Proyecto/controlador/asistencias/controlador_modificar_asistencias.php
+++ Proyecto/controlador/asistencias/controlador_modificar_asistencias.php
@@ -1,12 +1,12 @@
 <?php
     require '../../modelo/modelo_asistencias.php';
     $MD = new Modelo_Asistencias();
+
     $dia = htmlspecialchars($_POST['dia'],ENT_QUOTES,'UTF-8');
     $asistencia = htmlspecialchars($_POST['asistencia'],ENT_QUOTES,'UTF-8');
  
     $consulta = $MD->Asistencias_Modificar($dia,$asistencia);  
-
-      
     echo $consulta ;
-  
+  ?>
+
    

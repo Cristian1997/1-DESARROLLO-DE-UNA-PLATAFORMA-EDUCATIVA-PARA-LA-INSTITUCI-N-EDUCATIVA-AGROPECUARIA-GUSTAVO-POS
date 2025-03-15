34c49030987ee59dfd81af1e733282e0a11de382
Cristian E <66037173+Cristian1997@users.noreply.github.com>
2025-03-15 00:29:01 -0500

    Update controlador_combo_talleres_verificar_listar.php

--- Proyecto/controlador/trabajos/controlador_combo_talleres_verificar_listar.php
+++ Proyecto/controlador/trabajos/controlador_combo_talleres_verificar_listar.php
@@ -5,3 +5,4 @@ $id = htmlspecialchars($_POST['id'],ENT_QUOTES,'UTF-8');
 $id_grupo = htmlspecialchars($_POST['id_grupo'],ENT_QUOTES,'UTF-8');
 $consulta = $MS->listar_combo_talleres_verificar($id,$id_grupo);
 echo json_encode($consulta);
+?>

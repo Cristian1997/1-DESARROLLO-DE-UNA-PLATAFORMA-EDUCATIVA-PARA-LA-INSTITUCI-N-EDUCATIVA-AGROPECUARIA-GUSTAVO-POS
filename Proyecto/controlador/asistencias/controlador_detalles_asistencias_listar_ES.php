91e1db142c4e5f37b5e1831c202e4f12f055bd76
Cristian E <66037173+Cristian1997@users.noreply.github.com>
2025-03-15 00:02:24 -0500

    Update controlador_detalles_asistencias_listar_ES.php

--- Proyecto/controlador/asistencias/controlador_detalles_asistencias_listar_ES.php
+++ Proyecto/controlador/asistencias/controlador_detalles_asistencias_listar_ES.php
@@ -5,6 +5,7 @@
     $id_ES= htmlspecialchars($_POST['id_ES'],ENT_QUOTES,'UTF-8');
     $id_curso = htmlspecialchars($_POST['id_curso'],ENT_QUOTES,'UTF-8');
     $consulta = $MU->listar_deatelles_asistencias_ES($id_ES,$id_curso);
+
     if($consulta){
         echo json_encode($consulta);
     }else{
@@ -16,3 +17,4 @@
 		}';
     }
 
+?>

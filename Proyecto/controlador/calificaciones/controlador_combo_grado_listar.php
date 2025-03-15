c606d6d3fddebdd3f7fb01bfcd3c9203ebdad7c2
Cristian E <66037173+Cristian1997@users.noreply.github.com>
2025-03-15 00:09:06 -0500

    Update controlador_combo_grado_listar.php

--- Proyecto/controlador/calificaciones/controlador_combo_grado_listar.php
+++ Proyecto/controlador/calificaciones/controlador_combo_grado_listar.php
@@ -1,7 +1,10 @@
 <?php
     require '../../modelo/modelo_calificaciones.php';
     $MS = new Modelo_Calificaciones();
+
     $id = htmlspecialchars($_POST['id'],ENT_QUOTES,'UTF-8');
     $id_grupo = htmlspecialchars($_POST['id_grupo'],ENT_QUOTES,'UTF-8');
+
     $consulta = $MS->listar_combo_grado($id,$id_grupo);
     echo json_encode($consulta);
+?>

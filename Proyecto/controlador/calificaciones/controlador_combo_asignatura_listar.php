4361aca1c00b54fca137769063ace5fc1f619341
Cristian E <66037173+Cristian1997@users.noreply.github.com>
2025-03-15 00:05:10 -0500

    Update controlador_combo_asignatura_listar.php

--- Proyecto/controlador/calificaciones/controlador_combo_asignatura_listar.php
+++ Proyecto/controlador/calificaciones/controlador_combo_asignatura_listar.php
@@ -1,7 +1,10 @@
 <?php
 require '../../modelo/modelo_calificaciones.php';
 $MS = new Modelo_Calificaciones();
+
 $id = htmlspecialchars($_POST['id'],ENT_QUOTES,'UTF-8');
 $id_asignatura = htmlspecialchars($_POST['id_asignatura'],ENT_QUOTES,'UTF-8');
+
 $consulta = $MS->listar_combo_asignatura($id,$id_asignatura);
 echo json_encode($consulta);
+?>

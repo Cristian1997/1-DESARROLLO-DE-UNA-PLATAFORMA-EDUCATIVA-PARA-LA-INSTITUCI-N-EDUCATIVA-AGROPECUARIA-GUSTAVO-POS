e21eccac1b5c6437a1608e215e65a94b7bb96c47
Cristian E <66037173+Cristian1997@users.noreply.github.com>
2025-03-15 00:05:33 -0500

    Update controlador_calificaciones_listar.php

--- Proyecto/controlador/calificaciones/controlador_calificaciones_listar.php
+++ Proyecto/controlador/calificaciones/controlador_calificaciones_listar.php
@@ -7,6 +7,7 @@
     $id_grado = htmlspecialchars($_POST['id_grado'],ENT_QUOTES,'UTF-8');
     $id_grupo = htmlspecialchars($_POST['id_grupo'],ENT_QUOTES,'UTF-8');
     $id_docente = htmlspecialchars($_POST['id_docente'],ENT_QUOTES,'UTF-8');
+
     $consulta = $MU->listar_calificaciones($id_asignatura,$id_grado,$id_usuario_doc,$id_grupo,$id_docente);
     if($consulta){
         echo json_encode($consulta);

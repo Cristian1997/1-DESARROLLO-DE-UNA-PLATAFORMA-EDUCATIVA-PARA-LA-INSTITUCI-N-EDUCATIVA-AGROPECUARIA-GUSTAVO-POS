60e5c13a0361b13ee1681aeb853ed79bfab6eaad
Cristian E <66037173+Cristian1997@users.noreply.github.com>
2025-03-14 20:49:55 -0500

    Update conferencias.php

--- Proyecto/vista/video_conferencia/conferencias.php
+++ Proyecto/vista/video_conferencia/conferencias.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <link rel="stylesheet" type="text/css" href="conferencia.css">
     <title>EducaNet | Dashboard</title>
@@ -7,7 +8,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <!-- google fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Lobster+Two&display=swap" rel="stylesheet">
@@ -16,11 +18,14 @@
     <!-- SweetAlert2 -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 </head>
+
 <body>
     <div class="thebody text-center">
         <div class="container align-items-center " style="margin-top: 1%;">
-            <button id="start" class="btn btn-light btn-lg" type="button" onclick="iniciarNuevaVideoLlamada()"><b>Iniciar nueva video llamada</b></button>
-            <button id="ingresarConEnlaceAnterior" class="btn btn-light btn-lg" type="button" onclick="ingresarConEnlaceAnterior()"><b>Ingresar con enlace anterior</b></button>
+            <button id="start" class="btn btn-light btn-lg" type="button"
+                onclick="iniciarNuevaVideoLlamada()"><b>Iniciar nueva video llamada</b></button>
+            <button id="ingresarConEnlaceAnterior" class="btn btn-light btn-lg" type="button"
+                onclick="ingresarConEnlaceAnterior()"><b>Ingresar con enlace anterior</b></button>
         </div>
     </div>
 
@@ -65,83 +70,83 @@
         window.addEventListener('resize', () => {
             if (api) {
                 api.executeCommand('avatarUrl', 'your_avatar_url');
-                api.resize(window.innerWidth > 768 ? 1100 : window.innerWidth - 20, window.innerWidth > 768 ? 500 : 200);
+                api.resize(window.innerWidth > 768 ? 1100 : window.innerWidth - 20, window.innerWidth > 768 ?
+                    500 : 200);
             }
         });
     }
 
     function ingresarConEnlaceAnterior() {
-    $('#roomNameModal').modal('show');
-}
-
-function ingresarConEnlaceModal() {
-    var fullRoomName = document.getElementById('roomNameInput').value;
-
-    // Extraer el nombre de sala después de 'https://meet.jit.si/'
-    var roomName = fullRoomName.replace('https://meet.jit.si/', '');
-
-    if (roomName.length < 50) {
-        Swal.fire({
-            icon: 'error',
-            title: 'Error',
-            text: 'Ingrese un nombre de sala válido',
-        });
-        return;
+        $('#roomNameModal').modal('show');
     }
 
-    ocultarBotones();
+    function ingresarConEnlaceModal() {
+        var fullRoomName = document.getElementById('roomNameInput').value;
 
-    var domain = "meet.jit.si";
-    var options = {
-        roomName: roomName,
-        parentNode: container,
-        width: window.innerWidth > 768 ? 1100 : window.innerWidth - 20,
-        height: window.innerWidth > 768 ? 500 : 550,
-        configOverwrite: {
-            startWithAudioMuted: true,
-            startWithVideoMuted: true
+        // Extraer el nombre de sala despues de 'https://meet.jit.si/'
+        var roomName = fullRoomName.replace('https://meet.jit.si/', '');
+
+        if (roomName.length < 50) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Ingrese un nombre de sala válido',
+            });
+            return;
         }
-    };
 
-    api = new JitsiMeetExternalAPI(domain, options);
+        ocultarBotones();
 
-    window.addEventListener('resize', () => {
-        if (api) {
-            api.executeCommand('avatarUrl', 'your_avatar_url');
-            api.resize(window.innerWidth > 768 ? 1100 : window.innerWidth - 20, window.innerWidth > 768 ? 500 : 200);
-        }
-    });
+        var domain = "meet.jit.si";
+        var options = {
+            roomName: roomName,
+            parentNode: container,
+            width: window.innerWidth > 768 ? 1100 : window.innerWidth - 20,
+            height: window.innerWidth > 768 ? 500 : 550,
+            configOverwrite: {
+                startWithAudioMuted: true,
+                startWithVideoMuted: true
+            }
+        };
 
-    $('#roomNameModal').modal('hide');
-}
+        api = new JitsiMeetExternalAPI(domain, options);
 
+        window.addEventListener('resize', () => {
+            if (api) {
+                api.executeCommand('avatarUrl', 'your_avatar_url');
+                api.resize(window.innerWidth > 768 ? 1100 : window.innerWidth - 20, window.innerWidth > 768 ?
+                    500 : 200);
+            }
+        });
 
- 
-
+        $('#roomNameModal').modal('hide');
+    }
     </script>
 
 
 
-<!-- Agrega este HTML para el modal -->
-<div class="modal fade" id="roomNameModal" tabindex="-1" role="dialog" aria-labelledby="roomNameModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="roomNameModalLabel">Ingrese el enlace de la sala de conferencias</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <input type="text" id="roomNameInput" class="form-control" placeholder="Ingrese el enlace de la sala">
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-        <button type="button" class="btn btn-primary" onclick="ingresarConEnlaceModal()">Ingresar</button>
-      </div>
+    <!-- HTML para el modal -->
+    <div class="modal fade" id="roomNameModal" tabindex="-1" role="dialog" aria-labelledby="roomNameModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="roomNameModalLabel">Ingrese el enlace de la sala de conferencias</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <input type="text" id="roomNameInput" class="form-control"
+                        placeholder="Ingrese el enlace de la sala">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                    <button type="button" class="btn btn-primary" onclick="ingresarConEnlaceModal()">Ingresar</button>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
 
 
 
@@ -149,12 +154,15 @@ function ingresarConEnlaceModal() {
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
-
-
-
-

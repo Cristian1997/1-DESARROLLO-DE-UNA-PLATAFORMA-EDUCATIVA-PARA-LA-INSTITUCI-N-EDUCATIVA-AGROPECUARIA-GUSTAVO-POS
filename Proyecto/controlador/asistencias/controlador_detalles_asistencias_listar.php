43347e006a16b1cc40e2833fb67ba795a27610a3
Cristian E <66037173+Cristian1997@users.noreply.github.com>
2025-03-15 00:01:57 -0500

    Update controlador_detalles_asistencias_listar.php

--- Proyecto/controlador/asistencias/controlador_detalles_asistencias_listar.php
+++ Proyecto/controlador/asistencias/controlador_detalles_asistencias_listar.php
@@ -6,6 +6,7 @@
     $id_estudiantes = htmlspecialchars($_POST['id_estudiantes'],ENT_QUOTES,'UTF-8');
     $id_asignatura = htmlspecialchars($_POST['id_asignatura'],ENT_QUOTES,'UTF-8');
     $id_grado = htmlspecialchars($_POST['id_grado'],ENT_QUOTES,'UTF-8');
+
     $consulta = $MU->listar_deatelles_asistencias($id_asignatura,$id_grado,$id_usuario_doc,$id_estudiantes);
     if($consulta){
         echo json_encode($consulta);
@@ -17,4 +18,5 @@
 		    "aaData": []
 		}';
     }
+?>
 

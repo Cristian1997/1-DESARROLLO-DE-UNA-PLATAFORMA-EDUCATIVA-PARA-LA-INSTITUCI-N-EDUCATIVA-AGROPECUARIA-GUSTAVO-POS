4716c7b127e2fdfe17a4da6934e9287d1a85478b
Cristian E <66037173+Cristian1997@users.noreply.github.com>
2025-03-15 12:02:10 -0500

    Update vista_videoconferencia_clases_estudiantes_listar.php

--- Proyecto/vista/videoconferencias/vista_videoconferencia_clases_estudiantes_listar.php
+++ Proyecto/vista/videoconferencias/vista_videoconferencia_clases_estudiantes_listar.php
@@ -20,62 +20,58 @@
 
             <div class="form-group">
 
-
-
                 <div class="col-lg-6">
                     <label for="">Mis Grupos</label>
                     <select class="js-example-basic-single1" name="state" id="cbm_grupo" style="width:100%;">
 
-                    </select><br><br>
-
+                    </select>
+                    <br>
+                    <br>
                 </div>
 
                 <div class="col-lg-3">
-                 <label for="">&nbsp;</label><br>
-                 <button class="btn btn-primary" style="width:100%" onclick="listar_datos()" ><i class="fa fa-search"></i>  Listar</button>
+                    <label for="">&nbsp;</label>
+                    <br>
+                    <button class="btn btn-primary" style="width:100%" onclick="listar_datos()"><i class="fa fa-search"></i> Listar</button>
+                </div>
 
-             </div>   
-
-             <div class="col-lg-3">
-            <label for="">&nbsp;</label><br>
-            <button class="btn btn-success" style="width:100%"  onclick="window.open('video_conferencia/conferenciasingreso.php', '_blank');"><i class="fa fa-commenting-o"></i>&nbsp;  Iniciar video llamada</button>
-       </div> 
-
-
-
-         </div><br><br><br>
-         <div class="col-lg-12 table-responsive ">
-             <table id="tabla_videoconferencia_clases" class=" display  nowrap text-center" style="width:100%">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Fecha</th>
-                        <th>Links</th>
-                        <th>Docente</th>
-                        <th>Estado</th>
-                    </tr>
-                </thead>
-                <tfoot>
-                    <tr>
-                      <th>#</th>
-                      <th>Fecha</th>
-                      <th>Links</th>
-                      <th>Docente</th>
-                      <th>Estado</th>
-                  </tr>
-              </tfoot>
-          </table>
-      </div>
-      <!-- /.box-body -->
-  </div>
-  <!-- /.box -->
+                <div class="col-lg-3">
+                    <label for="">&nbsp;</label>
+                    <br>
+                    <button class="btn btn-success" style="width:100%" onclick="window.open('video_conferencia/conferenciasingreso.php', '_blank');"><i class="fa fa-commenting-o"></i>&nbsp; Iniciar video llamada</button>
+                </div>
+                
+            </div>
+            <br>
+            <br>
+            <br>
+            <div class="col-lg-12 table-responsive ">
+                <table id="tabla_videoconferencia_clases" class=" display  nowrap text-center" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Fecha</th>
+                            <th>Links</th>
+                            <th>Docente</th>
+                            <th>Estado</th>
+                        </tr>
+                    </thead>
+                    <tfoot>
+                        <tr>
+                            <th>#</th>
+                            <th>Fecha</th>
+                            <th>Links</th>
+                            <th>Docente</th>
+                            <th>Estado</th>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+            <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+    </div>
 </div>
-</div>
-
-
-
-
-
 
 <script src="../Plantilla/plugins/select2/select2.min.js"></script>
 <script>
@@ -87,10 +83,6 @@
      listar_combo_materia();
      listar_videoconferencia_clases();
      listar();
-
-
-
-
 
      document.getElementById('txt_archivo').addEventListener("change" , () => {
         var FileName  = document.getElementById('txt_archivo').value;
@@ -113,14 +105,9 @@
         }else{
           document.getElementById('txt_archivo_editar').value = "";
           Swal.fire("Mensaje De Advertencia", "SOLO SE ACEPTA IMAGENES - USTED SUBIO UN ARCHIVO CON EXTENCION "+exfile,"warning");
-
       }
-
   });  
-
  } );
-
-
 
     $('.box').boxWidget({
         animationSpeed : 500,
@@ -130,8 +117,7 @@
         expandIcon     :    'fa-plus',
         removeIcon     :    'fa-times'
     })
+    
     $(document).ready(function(){
-
-
     })
 </script>

29a8aa528b8b849c4c6b72367cb09e4abdba5835
Cristian E <66037173+Cristian1997@users.noreply.github.com>
2025-03-15 00:07:11 -0500

    Update controlador_combo_estudiante_verificar_listar.php

--- Proyecto/controlador/calificaciones/controlador_combo_estudiante_verificar_listar.php
+++ Proyecto/controlador/calificaciones/controlador_combo_estudiante_verificar_listar.php
@@ -1,6 +1,9 @@
 <?php
 require '../../modelo/modelo_calificaciones.php';
 $MS = new Modelo_Calificaciones();
+
 $id = htmlspecialchars($_POST['id'],ENT_QUOTES,'UTF-8');
+
 $consulta = $MS->listar_combo_estudiante_verifiar($id);
 echo json_encode($consulta);
+?>

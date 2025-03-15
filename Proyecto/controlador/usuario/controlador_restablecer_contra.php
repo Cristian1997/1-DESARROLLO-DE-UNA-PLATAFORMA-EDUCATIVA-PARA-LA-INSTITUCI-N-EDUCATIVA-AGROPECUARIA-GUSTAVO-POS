e4fc47e9d09b4b355d425989de6be45f85aab38c
Cristian E <66037173+Cristian1997@users.noreply.github.com>
2025-03-14 23:43:29 -0500

    Update controlador_restablecer_contra.php

--- Proyecto/controlador/usuario/controlador_restablecer_contra.php
+++ Proyecto/controlador/usuario/controlador_restablecer_contra.php
@@ -33,13 +33,13 @@ require 'PHPMailer/SMTP.php';
          $mail->isSMTP();                                            // Send using SMTP
          $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
          $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-         $mail->Username='educanetmantenimiento@gmail.com';//este debe ir en el address?
-         $mail->Password='mmhlffgfzwgjqypn';                            // SMTP password
+         $mail->Username='tu coreosoportetecnico@gmail.com';//este debe ir en el address?
+         $mail->Password='clave api';                            // SMTP password
          $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
          $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
          //Recipients
-         $mail->setFrom('educanetmantenimiento@gmail.com', 'Soporte EducaNet');
+         $mail->setFrom('tu coreosoportetecnico@gmail.com', 'Soporte EducaNet');
          $mail->addAddress($email);     // Add a recipient
 
          // Content

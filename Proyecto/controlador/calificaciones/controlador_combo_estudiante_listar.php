719ff412a8c8d21b553cbc33baab8b871fd87a93
Cristian E <66037173+Cristian1997@users.noreply.github.com>
2025-03-15 00:06:55 -0500

    Update controlador_combo_estudiante_listar.php

--- Proyecto/controlador/calificaciones/controlador_combo_estudiante_listar.php
+++ Proyecto/controlador/calificaciones/controlador_combo_estudiante_listar.php
@@ -1,8 +1,11 @@
 <?php
     require '../../modelo/modelo_calificaciones.php';
     $MS = new Modelo_Calificaciones();
+
     $id_grado = htmlspecialchars($_POST['id_grado'],ENT_QUOTES,'UTF-8');
     $id_asignatura = htmlspecialchars($_POST['id_asignatura'],ENT_QUOTES,'UTF-8');
     $id_docente = htmlspecialchars($_POST['id_docente'],ENT_QUOTES,'UTF-8');
+
     $consulta = $MS->listar_combo_estudiante($id_grado,$id_asignatura,$id_docente);
     echo json_encode($consulta);
+?>

f147873ab6a568a96560246bc79a7f9dc4656b37
Cristian E <66037173+Cristian1997@users.noreply.github.com>
2025-03-15 00:01:38 -0500

    Update controlador_combo_estudiantes_listar.php

--- Proyecto/controlador/asistencias/controlador_combo_estudiantes_listar.php
+++ Proyecto/controlador/asistencias/controlador_combo_estudiantes_listar.php
@@ -2,8 +2,11 @@
 <?php
 require '../../modelo/modelo_asistencias.php';
 $MA = new Modelo_Asistencias();
+
 $id_usuario_doc = htmlspecialchars($_POST['id_usuario_doc'],ENT_QUOTES,'UTF-8');
 $id_asignatura = htmlspecialchars($_POST['id_asignatura'],ENT_QUOTES,'UTF-8');
 $id_grado = htmlspecialchars($_POST['id_grado'],ENT_QUOTES,'UTF-8');
+
 $consulta = $MA->listar_combo_estudiantes($id_usuario_doc,$id_asignatura,$id_grado);
 echo json_encode($consulta);
+?>
